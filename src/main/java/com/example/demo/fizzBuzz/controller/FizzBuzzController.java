2400cafc21da60c521fbbe2fc1a534b721d6fa05
Iwase Yuzuki <y-iwse@seassist.co.jp>
2025-01-28 11:12:53 +0900

    Spring_Project commit

--- src/main/java/com/example/demo/fizzBuzz/controller/FizzBuzzController.java
+++ src/main/java/com/example/demo/fizzBuzz/controller/FizzBuzzController.java
@@ -1,5 +1,25 @@
 package com.example.demo.fizzBuzz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.fizzBuzz.service.FizzBuzzService;
+
+@Controller
 public class FizzBuzzController {
+
+	private final FizzBuzzService fizzBuzzService;
+
+	//依存性の注入
+	public FizzBuzzController(FizzBuzzService fizzBuzzService) {
+		this.fizzBuzzService = fizzBuzzService;
+	}
+
+	@GetMapping("/fizzbuzz")
+	public String getFizzBuzz(Model model) {
+		model.addAttribute("fizzBuzzList", fizzBuzzService.getFizzBuzzList());
+		return "fizzBuzz";
+	}
 
 }
--- src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
+++ src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
@@ -1,5 +1,28 @@
 package com.example.demo.fizzBuzz.service;
 
-public class FizzBuzzService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class FizzBuzzService {
+	public List<String> getFizzBuzzList() {
+		List<String> fizzBuzzList = new ArrayList<>();
+		fizzBuzzList.add("FizzBuzz");
+
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) {
+				fizzBuzzList.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				fizzBuzzList.add("Fizz");
+			} else if (i % 5 == 0) {
+				fizzBuzzList.add("Buzz");
+			} else {
+				fizzBuzzList.add(String.valueOf(i));
+			}
+		}
+		return fizzBuzzList;
+
+	}
 }
